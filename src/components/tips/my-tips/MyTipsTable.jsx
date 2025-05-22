e439081e70f04f79938526106662d1e00fc1fa63
Mahedul-Karim <karimrupo21@gmail.com>
2025-05-22 10:15:22 +0600

    tips deletion feature added

--- src/components/tips/my-tips/MyTipsTable.jsx
+++ src/components/tips/my-tips/MyTipsTable.jsx
@@ -18,31 +18,73 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
 import TipsForm from "../TipsForm";
+import { toast } from "sonner";
+import useServer from "@/hooks/useServer";
 
 const MyTipsTable = ({ tips = [] }) => {
   const [open, setOpen] = useState(false);
   const [data, setData] = useState(null);
 
+  const [openDelete, setOpenDelete] = useState(false);
+  const [deletionId, setDeletionId] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   const [allTips, setAllTips] = useState([...tips]);
+
+  const { callFetch } = useServer();
+
+  const handleDeletion = async () => {
+    try {
+      setIsLoading(true);
+
+      const options = {
+        method: "DELETE",
+      };
+
+      const res = await callFetch(`tips/${deletionId}`, options);
+
+      const newData = [...allTips].filter((tip) => tip._id !== deletionId);
+
+      setAllTips(newData);
+      toast.success(res.message);
+      setOpenDelete(false);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
       <div className="overflow-x-auto border border-solid border-border rounded-xl">
-        <Table className="table-fixed">
-          <TableHeader className="bg-foreground dark:bg-background rounded-xl">
-            <TableRow className="h-13">
-              <TableHead className="w-[250px] px-5">Title</TableHead>
-              <TableHead className="w-[120px]">Category</TableHead>
-              <TableHead className="w-[100px]">Availability</TableHead>
-              <TableHead className={"w-[80px]"}>Liked</TableHead>
-              <TableHead className="w-[100px]">Difficulty</TableHead>
-              <TableHead className="w-[80px]"></TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {allTips.length > 0 &&
-              allTips.map((tip) => (
+        {allTips?.length > 0 && (
+          <Table className="table-fixed">
+            <TableHeader className="bg-foreground dark:bg-background rounded-xl">
+              <TableRow className="h-13">
+                <TableHead className="w-[250px] px-5">Title</TableHead>
+                <TableHead className="w-[120px]">Category</TableHead>
+                <TableHead className="w-[100px]">Availability</TableHead>
+                <TableHead className={"w-[80px]"}>Liked</TableHead>
+                <TableHead className="w-[100px]">Difficulty</TableHead>
+                <TableHead className="w-[80px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {allTips.map((tip) => (
                 <TableRow key={tip._id}>
                   <TableCell className={"py-4"}>
                     <div className="flex items-center gap-2">
@@ -96,6 +138,10 @@ const MyTipsTable = ({ tips = [] }) => {
                       <Button
                         variant={"ghost"}
                         className={"text-dark hover:scale-[1.2]"}
+                        onClick={() => {
+                          setOpenDelete(true);
+                          setDeletionId(tip?._id);
+                        }}
                       >
                         <Trash />
                       </Button>
@@ -103,10 +149,17 @@ const MyTipsTable = ({ tips = [] }) => {
                   </TableCell>
                 </TableRow>
               ))}
-          </TableBody>
-        </Table>
+            </TableBody>
+          </Table>
+        )}
       </div>
-      <Dialog open={open} onOpenChange={setOpen}>
+      <Dialog
+        open={open}
+        onOpenChange={(val) => {
+          setOpen(val);
+          setData(null);
+        }}
+      >
         <DialogContent className="bg-background dark:bg-foreground border-border h-[80vh] overflow-auto">
           <DialogHeader>
             <DialogTitle className="font-semibold text-dark text-center xs:text-2xl text-xl">
@@ -127,6 +180,33 @@ const MyTipsTable = ({ tips = [] }) => {
           </div>
         </DialogContent>
       </Dialog>
+      <AlertDialog
+        open={openDelete}
+        onOpenChange={(val) => {
+          setOpenDelete(val);
+          setDeletionId(null);
+        }}
+      >
+        <AlertDialogContent className="bg-background dark:bg-foreground border-border">
+          <AlertDialogHeader>
+            <AlertDialogTitle className="text-dark">
+              Are you absolutely sure?
+            </AlertDialogTitle>
+            <AlertDialogDescription className="text-muted">
+              This action cannot be undone. This will permanently delete your
+              tip and remove your tip from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel className="border-border">
+              Cancel
+            </AlertDialogCancel>
+            <Button onClick={handleDeletion} disabled={isLoading}>
+              Continue
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   );
 };
